0b897c27143b7d23dfc2449cd907ad88170d91f9
Ana Carolina <ana.vasconcelos@ccc.ufcg.edu.br>
2019-06-03 22:47:37 -0300

    Testes da Classe ControllerPartido e Classe Partido atualizados

--- tests/lp2/ControllerPartidoTest.java
+++ tests/lp2/ControllerPartidoTest.java
@@ -4,19 +4,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class ControllerPartidoTest {
     private ControllerPartido controle;
 
     @Test
     void cadastraPartidoSemRepeticao() {
-        assertEquals(true, this.controle.cadastraPartido("NILSO"));
-        assertEquals(true, this.controle.cadastraPartido("LOUNGE"));
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("LOUNGE");
+        assertEquals("NILSO,LOUNGE", this.controle.exibirBase());
+
     }
 
     @Test
     void cadastraPartidoRepetidos() {
-        assertEquals(true, this.controle.cadastraPartido("NILSO"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido("NILSO"));
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("NILSO");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("NILSO", this.controle.exibirBase());
 
     }
 
@@ -26,4 +35,38 @@ class ControllerPartidoTest {
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido(null));
 
     }
+
+    @Test
+    void exibirBasePartidoRepitido() {
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("NILSO");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("NILSO", this.controle.exibirBase());
+    }
+
+    @Test
+    void exibirBasePartidoInvalido() {
+
+        assertEquals("", this.controle.exibirBase());
+        controle.cadastraPartido("");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("", this.controle.exibirBase());
+        controle.cadastraPartido(null);
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("", this.controle.exibirBase());
+
+    }
+    @Test
+    void exibirBasePartidoSemRepeticao() {
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("LOUNGE");
+        assertEquals("NILSO,LOUNGE", this.controle.exibirBase());
+    }
+
+
+
 }
