baf82fcbbb2890ea345256e962efa3c8abb2655b
Luciano Erick <luciano.erick.filho@ccc.ufcg.edu.br>
2019-06-03 23:42:41 -0300

    Testes de unidade adicionados.

--- tests/lp2/ControllerPartidoTest.java
+++ tests/lp2/ControllerPartidoTest.java
@@ -4,26 +4,66 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class ControllerPartidoTest {
     private ControllerPartido controle;
 
     @Test
     void cadastraPartidoSemRepeticao() {
-        assertEquals(true, this.controle.cadastraPartido("NILSO"));
-        assertEquals(true, this.controle.cadastraPartido("LOUNGE"));
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("LOUNGE");
+        assertEquals("NILSO,LOUNGE", this.controle.exibirBase());
+
     }
 
     @Test
     void cadastraPartidoRepetidos() {
-        assertEquals(true, this.controle.cadastraPartido("NILSO"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido("NILSO"));
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("NILSO");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("NILSO", this.controle.exibirBase());
 
     }
 
     @Test
     void cadastraPartidoVazioOuNulo() {
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido(""));
+     //   assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido(""));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPartido(null));
 
+    }
+
+    @Test
+    void exibirBasePartidoRepitido() {
+
+        controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("NILSO");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("NILSO", this.controle.exibirBase());
+    }
+
+    @Test
+    void exibirBasePartidoInvalido() {
+
+        assertEquals("", this.controle.exibirBase());
+        controle.cadastraPartido("");
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("", this.controle.exibirBase());
+        controle.cadastraPartido(null);
+        assertThrows(IllegalArgumentException.class, () -> this.controle.exibirBase());
+        assertEquals("", this.controle.exibirBase());
+
+    }
+    @Test
+    void exibirBasePartidoSemRepeticao() {
+
+        this.controle.cadastraPartido("NILSO");
+        assertEquals("NILSO", this.controle.exibirBase());
+        controle.cadastraPartido("LOUNGE");
+        assertEquals("NILSO,LOUNGE", this.controle.exibirBase());
     }
 }
--- tests/lp2/ControllerPessoaTest.java
+++ tests/lp2/ControllerPessoaTest.java
@@ -1,58 +1,70 @@
 package lp2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ControllerPessoaTest {
+
     private ControllerPessoa controle;
+
+    @BeforeEach
+    void setUp() {
+        this.controle = new ControllerPessoa();
+    }
 
     @Test
     void testCadastraPessoaSemPartido() {
-
-        assertEquals(true, controle.cadastraPessoa("José", "123564789-0", "PB", ""));
-        assertEquals(true, controle.cadastraPessoa("Maria", "987654321-0", "CE", "politica, novela"));
-        assertEquals(true, controle.cadastraPessoa("Pedro", "135797531-1", "RO", "futebol"));
+        controle.cadastraPessoa("Joao", "123564789-0", "PB", "");
+        controle.cadastraPessoa("Maria", "987654321-0", "CE", "politica, novela");
+        controle.cadastraPessoa("Pedro", "135797531-1", "RO", "futebol");
+        assertEquals("Joao - 123564789-0 (PB)", controle.exibirPessoa("123564789-0"));
+        assertEquals("Maria - 987654321-0 (CE) - Interesses: politica, novela", controle.exibirPessoa("987654321-0"));
+        assertEquals("Pedro - 135797531-1 (RO) - Interesses: futebol", controle.exibirPessoa("135797531-1"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("", "132321300-0", "PB", ""));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Luciano", "", "PB", ""));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Thayane", "132321300-4", "", "nao sei"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("José", "132321300,0", "PB", ""));
+        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Joao", "132321300,0", "PB", ""));
     }
 
     @Test
     void testCadastraPessoaComPartido() {
-        assertEquals(true, controle.cadastraPessoa("Daniel", "123645468-0", "SP", "", "PSTS"));
-        assertEquals(true, controle.cadastraPessoa("Paulo", "123456789-0", "RJ", "Social", "PRTS"));
-        assertEquals(true, controle.cadastraPessoa("Joaquim", "123321456-9", "PB", "", "JPM"));
+        controle.cadastraPessoa("Daniel", "123645468-0", "SP", "", "PSTS");
+        controle.cadastraPessoa("Paulo", "123456789-0", "RJ", "Social", "PRTS");
+        controle.cadastraPessoa("Joaquim", "123321456-9", "PB", "", "JPM");
+        assertEquals("Daniel - 123645468-0 (SP) - PSTS", controle.exibirPessoa("123645468-0"));
+        assertEquals("Paulo - 123456789-0 (RJ) - PRTS - Interesses: Social", controle.exibirPessoa("123456789-0"));
+        assertEquals("Joaquim - 123321456-9 (PB) - JPM", controle.exibirPessoa("123321456-9"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa(" ", "132321300-1", "PB", "", "EU"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Ana", "   ", "PB", "", "PSOL"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Luciano", "132321300-3", "    ", "", "EU"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraPessoa("Luciano", "132321300-4", "PB", "", "EU"));
     }
 
     @Test
     void testCadastraDeputado() {
-        assertEquals(true, controle.cadastraPessoa("José", "123564789-0", "PB", ""));
-        assertEquals(true, controle.cadastraPessoa("Daniel", "123645468-0", "SP", "", "PSTS"));
-        assertEquals(true, controle.cadastraPessoa("Paulo", "123456789-0", "RJ", "Social", "PRTS"));
-        assertEquals(true, controle.cadastraPessoa("Joaquim", "123321456-9", "PB", "", "JPM"));
 
-        assertEquals(true, controle.cadastraDeputado("123645468-0", "09102000"));
-        assertEquals(true, controle.cadastraDeputado("123456789-0", "13051986"));
+        controle.cadastraPessoa("José", "123564789-0", "PB", "");
+        assertEquals ("José - 123564789-0 (PB)", controle.exibirPessoa("123564789-0"));
+        controle.cadastraPessoa("Daniel", "123645468-0", "SP", "", "PSTS");
+        assertEquals ("Daniel - 123645468-0 (SP) - PSTS", controle.exibirPessoa("123645468-0"));
+        controle.cadastraPessoa("Paulo", "123456789-0", "RJ", "Social", "PRTS");
+        assertEquals ("Paulo - 123456789-0 (RJ) - PRTS - Interesses: Social", controle.exibirPessoa("123456789-0"));
+        controle.cadastraPessoa("Joaquim", "123321456-9", "PB", "", "JPM");
+        assertEquals ("Joaquim - 123321456-9 (PB) - JPM", controle.exibirPessoa("123321456-9"));
+
+        controle.cadastraDeputado("123645468-0", "09102000");
+        assertEquals ("POL: Daniel - 123645468-0 (SP) - PSTS - 09/10/2000 - 0 Leis", controle.exibirPessoa("123645468-0"));
+        controle.cadastraDeputado("123456789-0", "13051986");
+        assertEquals ("POL: Paulo - 123456789-0 (RJ) - PRTS - Interesses: Social - 13/05/1986 - 0 Leis", controle.exibirPessoa("123456789-0"));
 
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("bvdbv", "10111000"));
-
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("b8928b", "10111001"));
-
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("9999999-9", "10111002"));
-
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123321456-9", "evhhu"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123321456-9", "11111111111"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123321456-9", "35102000"));
         assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123321456-9", "20112020"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123645468-0", "20112001"));
-        assertThrows(IllegalArgumentException.class, () -> controle.cadastraDeputado("123564789-0", "20112002"));
     }
-
 }
