3159ae92dd9eaa220f637223acd0134c08c4e9e7
Luciano Erick <luciano.erick.filho@ccc.ufcg.edu.br>
2019-06-14 10:42:20 -0300

    Testes da US5 iniciados

--- src/ECO/Comissao/ControllerComissao.java
+++ src/ECO/Comissao/ControllerComissao.java
@@ -13,6 +13,10 @@ public class ControllerComissao {
         this.mapaComissoes = new HashMap<>();
     }
 
+    public Map<String, Comissao> getMapaComissoes() {
+        return mapaComissoes;
+    }
+
     public void cadastrarComissao(String tema, String dniPoliticos) {
         validadorString(tema, "Erro ao cadastrar comissao: tema nao pode ser vazio ou nulo");
         validadorString(dniPoliticos, "Erro ao cadastrar comissao: lista de politicos nao pode ser vazio ou nulo");
--- tests/ECO/ControllerGeralTest.java
+++ tests/ECO/ControllerGeralTest.java
@@ -5,6 +5,7 @@ import ECO.Comissao.ControllerComissao;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class ControllerGeralTest {
@@ -13,9 +14,27 @@ class ControllerGeralTest {
     @Test
     void cadastraComissaoValidaTest() {
         this.controleGeral.getControlePessoas().cadastraPessoa("Ana Carolina","111222-2","PB","Música","PT");
-        this.controleGeral.getControlePessoas().cadastraPessoa();
+        this.controleGeral.getControlePessoas().cadastraPessoa("Luciano Erick","6666-5","PB","Funk","BRASIL");
         this.controleGeral.getControleComissao().cadastrarComissao("NILSO","111222-2,6666-5");
-        assertEquals("NILSO",this.controleGeral.getControleComissao());
+
+        assertEquals("NILSO", this.controleGeral.getControleComissao().getMapaComissoes().get("NILSO").getTema());
+    }
+
+    @Test
+    void cadastraComissaoDniInvalidoTest() {
+
+        this.controleGeral.getControlePessoas().cadastraPessoa("Ana Carolina","111A22-2","PB","Música","PT");
+        this.controleGeral.getControlePessoas().cadastraPessoa("Luciano Erick","6*66-5","PB","Funk","BRASIL");
+
+        assertThrows(IllegalArgumentException.class, () -> this.controleGeral.getControleComissao().cadastrarComissao("NILSO","111A22-2,6*66-5"));
+    }
+
+    @Test
+    void cadastraComissaoTemaJaCadastrado() {
+        this.controleGeral.getControleComissao().cadastrarComissao("NILSO","1111-0,2222-9");
+        assertEquals("NILSO", this.controleGeral.getControleComissao().getMapaComissoes().get("NILSO").getTema());
+        this.controleGeral.getControleComissao().cadastrarComissao("NILSO","1111-0,2222-9");
+        assertThrows(IllegalArgumentException.class, () -> this.controleGeral.getControleComissao().cadastrarComissao("NILSO","111A22-2,6*66-5"));
 
 
 
