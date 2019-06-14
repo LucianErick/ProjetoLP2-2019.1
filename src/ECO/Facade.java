9621be6783fec28de9ecb1ac43ec652f5780124e
arturbs <arturbritosouza@hotmail.com.br>
2019-06-14 15:16:07 -0300

    US1 ate US4 OK

--- src/ECO/Facade.java
+++ src/ECO/Facade.java
@@ -48,7 +48,10 @@ public class Facade {
 
 
     public static void main(String[] args) {
-        args = new String[] { "ECO.Facade", "acceptance_tests/use_case_1.txt", "acceptance_tests/use_case_2.txt", "acceptance_tests/use_case_3.txt", "acceptance_tests/use_case_4.txt" };
+        args = new String[] { "ECO.Facade", "acceptance_tests/use_case_1.txt",
+                "acceptance_tests/use_case_2.txt", "acceptance_tests/use_case_3.txt",
+                "acceptance_tests/use_case_4.txt", "acceptance_tests/use_case_5.txt",
+                "acceptance_tests/use_case_6.txt" };
         EasyAccept.main(args);
     }
 }
