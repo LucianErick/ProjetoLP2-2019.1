a55469cc97acc347d8c674d40d0c030782364c7a
GitBerg <49658507+GitBerg@users.noreply.github.com>
2019-06-30 19:38:41 -0300

    Update ControllerPLS.java

--- src/ECO/PropostasLegislativas/ControllerPLS.java
+++ src/ECO/PropostasLegislativas/ControllerPLS.java
@@ -1,6 +1,8 @@
 package ECO.PropostasLegislativas;
 
 import ECO.Comissao.Comissao;
+import ECO.Pessoa.Deputado;
+import ECO.Pessoa.Pessoa;
 
 import java.io.*;
 import java.lang.reflect.Array;
@@ -15,146 +17,144 @@ public class ControllerPLS implements Serializable {
 //    private int numeroPLP = 1;
 //    private int numeroPEC = 1;
 
-    private HashMap<String, PropostaLegislativa> propostasDeLeis;
-    /**
-     * Identificacao das informacoes sobre as propostas legistativas depois da finalizacao da execucao do programa
-     */
-    private static final long serialVersionUID = 1L;
+	private HashMap<String, PropostaLegislativa> propostasDeLeis;
 
-    private HashMap<Integer, Integer> numeroPL;
-    private HashMap<Integer, Integer> numeroPLP;
-    private HashMap<Integer, Integer> numeroPEC;
+	private HashMap<Integer, Integer> numeroPL;
+	private HashMap<Integer, Integer> numeroPLP;
+	private HashMap<Integer, Integer> numeroPEC;
 
+	public ControllerPLS() {
+		this.propostasDeLeis = new HashMap<>();
+		this.numeroPL = new HashMap<>();
+		this.numeroPLP = new HashMap<>();
+		this.numeroPEC = new HashMap<>();
 
-    public ControllerPLS() {
-        this.propostasDeLeis = new HashMap<>();
-        this.numeroPL = new HashMap<>();
-        this.numeroPLP = new HashMap<>();
-        this.numeroPEC = new HashMap<>();
+	}
 
-    }
+	public String cadastrarPL(String dni, int ano, String ementa, String interesses, String url, boolean conclusivo) {
+		validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual");
+		validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
 
-    public String cadastrarPL(String dni, int ano, String ementa, String interesses, String url, boolean conclusivo){
-        validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
-        validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
-        validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
-        validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
-        validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
-        validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual" );
-        validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
+		String codigo = "PL " + contadorPL(ano) + "/" + ano;
+		propostasDeLeis.put(codigo, new PL(dni, ano, ementa, interesses, url, conclusivo, codigo));
+		return codigo;
+	}
 
-        String codigo = "PL " + contadorPL(ano) + "/" + ano;
-        propostasDeLeis.put(codigo,new PL(dni,ano,ementa,interesses,url,conclusivo, codigo));
-        return codigo;
-    }
+	public String cadastrarPLP(String dni, int ano, String ementa, String interesses, String url, String artigos) {
+		validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validadorString(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
+		validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual");
+		validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
 
-    public String cadastrarPLP(String dni, int ano, String ementa, String interesses, String url, String artigos){
-        validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
-        validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
-        validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
-        validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
-        validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
-        validadorString(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
-        validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual" );
-        validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
+		String codigo = "PLP " + contadorPLP(ano) + "/" + ano;
+		propostasDeLeis.put(codigo, new PLP(dni, ano, ementa, interesses, url, artigos, codigo));
+		return codigo;
 
-        String codigo = "PLP " + contadorPLP(ano) + "/" + ano;
-        propostasDeLeis.put(codigo,new PLP(dni,ano,ementa,interesses,url,artigos, codigo));
-        return codigo;
+	}
 
-    }
+	public String cadastrarPEC(String dni, int ano, String ementa, String interesses, String url, String artigos) {
+		validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validadorString(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
+		validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual");
+		validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
 
-    public String cadastrarPEC(String dni, int ano, String ementa, String interesses, String url, String artigos){
-        validadorString(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
-        validadorDni(dni, "Erro ao cadastrar projeto: dni invalido");
-        validadorString(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
-        validadorString(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
-        validadorString(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
-        validadorString(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
-        validadorAnoFuturo(ano, "Erro ao cadastrar projeto: ano posterior ao ano atual" );
-        validadorAno(ano, "Erro ao cadastrar projeto: ano anterior a 1988");
+		String codigo = "PEC " + contadorPEC(ano) + "/" + ano;
+		propostasDeLeis.put(codigo, new PEC(dni, ano, ementa, interesses, url, artigos, codigo));
+		return codigo;
+	}
 
-        String codigo = "PEC " + contadorPEC(ano) + "/" + ano;
-        propostasDeLeis.put(codigo,new PEC(dni,ano,ementa,interesses,url,artigos, codigo));
-        return codigo;
-    }
+	private Integer contadorPL(Integer ano) {
+		if (numeroPL.containsKey(ano)) {
+			numeroPL.put(ano, numeroPL.get(ano) + 1);
+			return numeroPL.get(ano);
+		} else {
+			numeroPL.put(ano, 0);
+			return contadorPL(ano);
+		}
+	}
 
-    private Integer contadorPL(Integer ano) {
-        if (numeroPL.containsKey(ano)) {
-            numeroPL.put(ano,numeroPL.get(ano) + 1);
-            return numeroPL.get(ano);
-        } else {
-            numeroPL.put(ano, 0);
-            return contadorPL(ano);
-        }
-    }
+	private Integer contadorPLP(Integer ano) {
+		if (numeroPLP.containsKey(ano)) {
+			numeroPLP.put(ano, numeroPLP.get(ano) + 1);
+			return numeroPLP.get(ano);
+		} else {
+			numeroPLP.put(ano, 0);
+			return contadorPLP(ano);
+		}
+	}
 
-    private Integer contadorPLP(Integer ano) {
-        if (numeroPLP.containsKey(ano)) {
-            numeroPLP.put(ano,numeroPLP.get(ano) + 1);
-            return numeroPLP.get(ano);
-        } else {
-            numeroPLP.put(ano, 0);
-            return contadorPLP(ano);
-        }
-    }
+	private Integer contadorPEC(Integer ano) {
+		if (numeroPEC.containsKey(ano)) {
+			numeroPEC.put(ano, numeroPEC.get(ano) + 1);
+			return numeroPEC.get(ano);
+		} else {
+			numeroPEC.put(ano, 0);
+			return contadorPEC(ano);
+		}
+	}
 
-    private Integer contadorPEC(Integer ano) {
-        if (numeroPEC.containsKey(ano)) {
-            numeroPEC.put(ano,numeroPEC.get(ano) + 1);
-            return numeroPEC.get(ano);
-        } else {
-            numeroPEC.put(ano, 0);
-            return contadorPEC(ano);
-        }
-    }
+	/**
+	 * Exibe o projeto que se refere a String codigo passada como parametro
+	 * 
+	 * @param codigo meio de identificacao do projeto
+	 * @return o toString do projeto relacionado
+	 */
+	public String exibirProjeto(String codigo) {
+		return propostasDeLeis.get(codigo).toString();
+	}
 
-    /**
-     * Exibe o projeto que se refere a String codigo passada como parametro
-     * @param codigo meio de identificacao do projeto
-     * @return o toString do projeto relacionado
-     */
-    public String exibirProjeto(String codigo){
-        return  propostasDeLeis.get(codigo).toString();
-    }
+	public boolean verificaBooleanConclusivo(String codigo) {
+		return this.propostasDeLeis.get(codigo).verificaBooleanConclusivo();
+	}
 
-    public boolean verificaBooleanConclusivo(String codigo) {
-        return this.propostasDeLeis.get(codigo).verificaBooleanConclusivo();
-    }
+	public HashMap<String, PropostaLegislativa> getPropostasDeLeis() {
+		return propostasDeLeis;
+	}
 
-    public HashMap<String, PropostaLegislativa> getPropostasDeLeis() {
-        return propostasDeLeis;
-    }
+	public String getInteressesRelacionados(String codigo) {
+		return propostasDeLeis.get(codigo).getInteressesRelacionados();
+	}
 
+	public void alteraSituacaoAtual(String codigo, String alteracao) {
+		propostasDeLeis.get(codigo).setSituacaoAtual(alteracao);
+	}
 
-    public String  getInteressesRelacionados(String codigo) {
-        return propostasDeLeis.get(codigo).getInteressesRelacionados();
-    }
+	public void quorumMininimo(String codigo, int deputadosPresentes, int totalDeDeputados) {
+		propostasDeLeis.get(codigo).quorumMininimo(deputadosPresentes, totalDeDeputados);
+	}
 
-    public void alteraSituacaoAtual (String codigo, String alteracao) {
-        propostasDeLeis.get(codigo).setSituacaoAtual(alteracao);
-    }
+	public String exibirTramitacao(String codigo) {
+		String complemento = "";
 
-    public void quorumMininimo (String codigo, int deputadosPresentes, int totalDeDeputados) {
-        propostasDeLeis.get(codigo).quorumMininimo(deputadosPresentes, totalDeDeputados);
-    }
-
-    public String exibirTramitacao(String codigo) {
-        String complemento = "";
-
-        if (!(propostasDeLeis.get(codigo).getSituacaoAtual().contains("ARQUIVADO")) && !(propostasDeLeis.get(codigo).getSituacaoAtual().contains("APROVADO"))
-                && !(propostasDeLeis.get(codigo).exibirTramitacao(codigo).equals("EM VOTACAO (CCJC)"))) {
-            complemento = ", " + propostasDeLeis.get(codigo).getSituacaoAtual();
+		if (!(propostasDeLeis.get(codigo).getSituacaoAtual().contains("ARQUIVADO"))
+				&& !(propostasDeLeis.get(codigo).getSituacaoAtual().contains("APROVADO"))
+				&& !(propostasDeLeis.get(codigo).exibirTramitacao(codigo).equals("EM VOTACAO (CCJC)"))) {
+			complemento = ", " + propostasDeLeis.get(codigo).getSituacaoAtual();
 //            proximoLocal = plenario, mas tramitacao tem que ser Plenario.
-            String complemento1[] = complemento.split("plenario");
-            if (complemento1.length == 2) {
-                complemento = ", EM VOTACAO (Plenario)";
-            }
-        }
-        return propostasDeLeis.get(codigo).exibirTramitacao(codigo) + complemento;
-    }
-    
-    public void configuraEstrategiaProposta(String dni, String estrategia) {
+			String complemento1[] = complemento.split("plenario");
+			if (complemento1.length == 2) {
+				complemento = ", EM VOTACAO (Plenario)";
+			}
+		}
+		return propostasDeLeis.get(codigo).exibirTramitacao(codigo) + complemento;
+	}
+	
+	public Map<String, String> estrategia ;
+	
+	public void configuraEstrategiaProposta(String dni, String estrategia) {
 		switch (estrategia) {
 		case "CONSTITUCIONAL":
 			break;
@@ -169,8 +169,15 @@ public class ControllerPLS implements Serializable {
 	}
 
 	public String pegarPropostaRelacionada(String dni, Map<String, Pessoa> pessoa,Map<String, PropostaLegislativa> leis) {
+		estrategia = new HashMap<>();
+		for(Map.Entry<String,Pessoa> entry2 : pessoa.entrySet()) {
+			this.estrategia.put(entry2.getKey(),"CONSTITUCIONAL");
+			System.out.println(this.estrategia);
+		}
+		
 		String retorno = "";
 		String[] interesses = pessoa.get(dni).getInteresses().trim().split(",");
+		if(this.estrategia.get(dni).equals("CONSTITUCIONAL")) {
 		for (Map.Entry<String, PropostaLegislativa> entry : leis.entrySet()) {
 			for (int i = 0; i < interesses.length; i++) {
 				if (interesses[i].equals(entry.getValue().getInteressesRelacionados())) {
@@ -188,66 +195,54 @@ public class ControllerPLS implements Serializable {
 			}
 			
 		}
+	}
 	return retorno;
 
 	}
 
-    /**
-     * Método de gravação da map de propostas legislativas, o objeto é gravado após o sistema ser fechado.
-     * @param map de propostas legislativas
-     * @param arquivo, nome do arquivo de destino.
-     */
+	public void escreverArquivos(Map<String, PropostaLegislativa> map, String arquivo) {
 
-    public void escreverArquivos(Map<String, PropostaLegislativa> map, String arquivo){
+		FileOutputStream arquivoPLS;
 
-        FileOutputStream arquivoPLS;
+		try {
+			arquivoPLS = new FileOutputStream(arquivo);
+			ObjectOutputStream gravarPLS = new ObjectOutputStream(arquivoPLS);
+			gravarPLS.writeObject(map);
+			gravarPLS.flush();
+			gravarPLS.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        try {
-            arquivoPLS = new FileOutputStream(arquivo);
-            ObjectOutputStream gravarPLS = new ObjectOutputStream(arquivoPLS);
-            gravarPLS.writeObject(map);
-            gravarPLS.flush();
-            gravarPLS.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	}
 
-    }
-    /**
-     * Método de recuperação de mapa gravado.
-     * @param arquivo, nome do arquivo onde os dados serão buscados.
-     * @return, retorna um mapa com as propostas legislativas gravado anteriormente.
-     */
+	public Map<String, PropostaLegislativa> lerArquivos(String arquivo) {
+		File arquivoPLS = null;
+		arquivoPLS = new File(arquivo);
+		Map<String, PropostaLegislativa> map = new HashMap<>();
+		FileInputStream fis;
 
-    public void lerArquivos (String arquivo){
-        File arquivoPLS = null;
-        arquivoPLS = new File(arquivo);
-        Map<String, PropostaLegislativa> map = new HashMap<>();
-        FileInputStream fis;
+		try {
+			if (!arquivoPLS.exists()) {
+				arquivoPLS.createNewFile();
+			} else if (arquivoPLS.length() == 0) {
+				System.out.println("ARQUIVO VAZIO");
 
-        try {
-            if (!arquivoPLS.exists()) {
-                arquivoPLS.createNewFile();
-            }
-            else if (arquivoPLS.length() == 0) {
-                System.out.println("ARQUIVO VAZIO");
+			} else {
+				fis = new FileInputStream(arquivo);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				map = (Map<String, PropostaLegislativa>) ois.readObject();
+				ois.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            }else{
-                fis = new FileInputStream(arquivo);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-                this.propostasDeLeis = (HashMap<String, PropostaLegislativa>) ois.readObject();
-                ois.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		return map;
 
-    }
+	}
 
-    /**
-     * Inicializa um novo mapa sem informacoes salvas
-     */
-    public void limpar() {
-        this.propostasDeLeis = new HashMap<>();
-    }
+	public void limpar() {
+		this.propostasDeLeis = new HashMap<>();
+	}
 }
